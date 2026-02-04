a883bd77d88468b936a85e1cc4e80cc89b7ea7b8
66010914017-maker <66010914017@msu.ac.th>
2026-02-04 14:46:01 +0700

    h

---
+++ h/e.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>วัชรพล เวชแพทย์(หนึ่ง)</title>
+</head>
+
+<body>
+<h1>e.php</h1>
+
+<?php
+	unset($_SESSION['name'] );
+	unset($_SESSION['p1'] );
+	
+?>
+
+</body>
+</html>
