5ab1159c60a83eed8f71ddde2d61038d4a23b640
watcharaphon <66010914017@msu.ac.th>
2026-02-18 16:48:06 +0700

    a

---
+++ k/a.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>วัชรพล เวชแพทย์ (หนึ่ง)</title>
+</head>
+
+<body>
+
+
+<h1>งาน k 66010914017 วัชรพล เวชแพทย์ (หนึ่ง)</h1>
+<br>
+<button><input type="color"></button><br>
+<button><input type="color"></button><br>
+</form>
+
+
+<body>
+</html>
